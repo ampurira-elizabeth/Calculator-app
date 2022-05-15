bcc418e01ff6d273b0a2ef355f3d0d40414f0794
ampuriraelizabeth <elizabethampurira@gmail.com>
2022-05-15 17:13:42 +0300

    first commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.calculator
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btnAdd:Button
+    lateinit var btnSubtract:Button
+    lateinit var btnMultiply:Button
+    lateinit var btnModulus:Button
+    lateinit var etNumber:EditText
+    lateinit var etNumber2:EditText
+    lateinit var tvadd:TextView
+    lateinit var tvminus:TextView
+    lateinit var tvtimes:TextView
+    lateinit var tvmodulus:TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        etNumber=findViewById(R.id.etNumber)
+        etNumber2=findViewById(R.id.etNumber2)
+        tvadd=findViewById(R.id.tvadd)
+        tvminus=findViewById(R.id.tvminus)
+        tvtimes=findViewById(R.id.tvtimes)
+        tvmodulus=findViewById(R.id.tvmodulus)
+
+
+        btnAdd=findViewById(R.id.btnAdd)
+        btnAdd.setOnClickListener {
+         val firstnumb=etNumber.text.toString()
+            val secondnumb=etNumber2.text.toString()
+
+            addition(firstnumb.toInt(), secondnumb.toInt())
+        }
+
+        btnSubtract=findViewById(R.id.btnSubtract)
+        btnSubtract.setOnClickListener {
+            val firstnumb=etNumber.text.toString()
+            val secondnumb=etNumber2.text.toString()
+
+            subtraction(firstnumb.toInt(), secondnumb.toInt())
+        }
+
+        btnMultiply=findViewById(R.id.btnMultiply)
+        btnMultiply.setOnClickListener {
+            val firstnumb=etNumber.text.toString()
+            val secondnumb=etNumber2.text.toString()
+
+            multiplication(firstnumb.toInt(), secondnumb.toInt())
+        }
+
+        btnModulus=findViewById(R.id.btnModulus)
+        btnModulus.setOnClickListener {
+            val firstnumb=etNumber.text.toString()
+            val secondnumb=etNumber2.text.toString()
+
+            division(firstnumb.toInt(), secondnumb.toInt())
+        }
+
+    }
+    fun addition(firsnumb:Int,secondnumb:Int){
+        var add=firsnumb+secondnumb
+        tvadd.text=add.toString()
+    }
+    fun subtraction(firsnumb:Int,secondnumb:Int){
+        var subtract=firsnumb-secondnumb
+        tvminus.text=subtract.toString()
+    }
+    fun multiplication(firsnumb:Int,secondnumb:Int){
+        var multiply=firsnumb*secondnumb
+        tvtimes.text=multiply.toString()
+    }
+    fun division(firsnumb:Int,secondnumb:Int){
+        var divide=firsnumb%secondnumb
+        tvmodulus.text=divide.toString()
+    }
+}
